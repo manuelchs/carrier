d6ae10c4ea63ffbf3bc1bab8caf034090513706f
Manu <manuelchavarin@aori.mx>
2021-06-11 20:06:18 -0500

    footer and more

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                         <p>Ofrecemos una gama completa en equipos de refrigeración para el transporte como camiones, trailers y contenedores.</p>
                         <div class="buttons-section">
                             <button class="main-button white-button"><span>Ver más</span></button>
-                            <button class="main-button green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
+                            <button class="main-button ml-1 green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
                         </div>
                     </div>
                     <div class="slider-section">
@@ -39,7 +39,7 @@
                         <p>Ofrecemos repuestos y accesorios de alta calidad diseñados para mantener su equipo funcionando al máximo.</p>
                         <div class="buttons-section">
                             <button class="main-button white-button"><span>Ver más</span></button>
-                            <button class="main-button green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
+                            <button class="main-button ml-1 green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
                         </div>
                     </div>
                     <div class="slider-section">
@@ -57,7 +57,7 @@
                         <p>Servicio técnico especializado y programas de mantenimiento preventivo para sus equipos Carrier Transicold.</p>
                         <div class="buttons-section">
                             <button class="main-button white-button"><span>Ver más</span></button>
-                            <button class="main-button green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
+                            <button class="main-button ml-1 green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
                         </div>
                     </div>
                     <div class="slider-section">
@@ -71,5 +71,190 @@
             <div id="point3" div="3" class="points-item p3"></div>
         </div>
     </div>
+    <div class="main-wrapper">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Nosotros somos</h3>
+                <h2>Carrier Transicold <span>Guadalajara</span></h2>
+            </div>
+            <div class="boxes-wrapper">
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-equipos.png" alt="">
+                    <div class="description">
+                        <h4>Equipos</h4>
+                        <p>Ofrecemos una gama completa en equipos de refrigeración para el transporte como camiones, trailers y contenedores.</p>
+                        <button>Ver equipos <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-refacciones.png" alt="">
+                    <div class="description">
+                        <h4>Refacciones</h4>
+                        <p>Ofrecemos repuestos y accesorios de alta calidad diseñados para mantener su equipo funcionando al máximo.</p>
+                        <button>Ver más <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-servicio.png" alt="">
+                    <div class="description">
+                        <h4>Servicio y mantenimiento</h4>
+                        <p>Servicio técnico especializado y programas de mantenimiento preventivo para sus equipos Carrier Transicold.</p>
+                        <button>Más información <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper big-wrapper distribuidor-wrapper">
+        <div class="general-max-width">
+            <div class="info-section half-section">
+                <div class="title-one-color">
+                    <h3>25 años de experiencia</h3>
+                    <h2>Distribuidor oficial Carrier Transicold</h2>
+                </div>
+                <p>Contamos con la línea completa de equipos y servicios para el transporte refrigerado.</p>
+                <div class="buttons-section">
+                    <button class="main-button green-button"><span>Contactar</span></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper beneficios-wrapper">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Beneficios exclusivos</h3>
+                <h2>¿Por qué elegirnos?</h2>
+            </div>
+            <div class="boxes-wrapper">
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-equipos.png" alt="">
+                    <div class="description">
+                        <h4>Servicio <span>especializado</span></h4>
+                        <p class="medium-gray-text">Ofrecemos una gama completa en equipos de refrigeración para el transporte como camiones, trailers y contenedores.</p>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-refacciones.png" alt="">
+                    <div class="description">
+                        <h4>Distribuidor <span>oficial</span></h4>
+                        <p class="medium-gray-text">Ofrecemos repuestos y accesorios de alta calidad diseñados para mantener su equipo funcionando al máximo.</p>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/home/servicios-servicio.png" alt="">
+                    <div class="description">
+                        <h4>Atención de <span>Garantías</span></h4>
+                        <p class="medium-gray-text">Servicio técnico especializado y programas de mantenimiento preventivo para sus equipos Carrier Transicold.</p>
+                    </div>
+                </a>
+            </div>
+            <p class="footer-section">Comunícate con nosotros y obtén la atención personalizada que estás buscando. <a>Contactar <i class="far fa-long-arrow-right"></i></a></p>
+        </div>
+    </div>
+
+    <div class="main-wrapper big-wrapper agendar-wrapper">
+        <div class="general-max-width">
+            <div class="info-section half-section">
+                <div class="title-one-color">
+                    <h3>Agenda ahora</h3>
+                    <h2>Programa tu servicio técnico con nosotros</h2>
+                </div>
+                <p>Servicio técnico especializado en refrigeración para camiones, tráilers, VCL y contenedores.</p>
+                <div class="buttons-section">
+                    <button class="main-button green-button icon icon-left"><i class="fas fa-phone"></i><span>Llamar</span></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper nuestros-clientes-wrapper">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Nuestro clientes</h3>
+                <h2>Trabajamos con los mejores</h2>
+            </div>
+
+            <div class="client-grid-wrapper">
+                <img src="/assets/images/home/clientes/cliente-19-hermanos.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-apolotran.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-berrymex.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-dubacano.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-dumy.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-giddings.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-grupo-tarahumara.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-marisa.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-omex.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-pilgrims.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-sello-rojo.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-soriana.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-sukarne.png" alt="">
+                <img src="/assets/images/home/clientes/cliente-thrifty.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper big-wrapper servicio-integral-wrapper">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Servicio integral</h3>
+                <h2>Todo lo que necesitas en<br><span>Refrigeración para el transporte</span></h2>
+                <button class="main-button green-button"><span>Contactar</span></button>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="gray-section">
+            <div class="general-max-width">
+                <div class="links-section">
+                    <div class="links-item">
+                        <h6>Empresa</h6>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                    </div>
+                    <div class="links-item">
+                        <h6>Empresa</h6>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                    </div>
+                    <div class="links-item">
+                        <h6>Empresa</h6>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                    </div>
+                    <div class="links-item">
+                        <h6>Empresa</h6>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                    </div>
+                    <div class="links-item">
+                        <h6>Empresa</h6>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                        <a href="#">Enlace</a>
+                    </div>
+                </div>
+                <div class="logo-section">
+    
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
