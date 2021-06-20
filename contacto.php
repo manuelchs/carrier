d0a978b7adac82dcf4b8c0eaa6fd789ca1b0a4ed
Manu <manuelchavarin@aori.mx>
2021-06-20 18:46:09 -0500

    Servicio tecnico

---
+++ contacto.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include('partial_views/head_tag.php') ?>
+    <title>Carrier Transicold - Contacto</title>
+</head>
+<body>
+    <?php $page = 'contacto'; ?>
+    <?php include('partial_views/navbar.php') ?>
+
+    <div class="hero">
+        <div class="general-max-width">
+            <div class="info">
+                <h3>Contacto</h3>
+                <h1>Centro de atención<br><span>Carrier Transicold GDL</span></h1>
+                <p>Si deseas comunicarte no dudes en llamarnos o dejarnos un mensaje, con gusto atenderemos cualquier duda, sugerencia o comentario que desees hacernos.</p>
+            </div>
+            <div class="img bottom-mobile">
+                <img src="/assets/images/servicio/tecnico-carrier-transicold.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper">
+        <div class="general-max-width">
+            <div class="blue-section">
+                <div class="title-centered">
+                    <h3>Escríbenos</h3>
+                    <h2>Queremos escucharte</h2>
+                </div>
+                <div class="data-contact">
+                    <div class="icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <p>
+                        <span>(33) 3849 2695</span>
+                        <span>Venta de equipos</span>
+                    </p>
+                </div>
+                <div class="data-contact">
+                    <div class="icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <p>
+                        <span>(33) 1329 8661</span>
+                        <span>Tlaquepaque, Jalisco.</span>
+                    </p>
+                </div>
+                <div class="data-contact">
+                    <div class="icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <p>
+                        <span>(33) 1137 0873</span>
+                        <span>Servicio en taller</span>
+                    </p>
+                </div>
+                <div class="data-contact">
+                    <div class="icon">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <p>
+                        <span>(33) 2612 2140</span>
+                        <span>Venta de reafacciones</span>
+                    </p>
+                </div>
+            </div>
+            <div class="form-wrapper">
+                <div class="title-centered">
+                    <h3>Escríbenos</h3>
+                    <h2>Queremos escucharte</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <?php include('partial_views/footer.php') ?>
+</body>
+</html>
--- servicio-tecnico.php
+++ servicio-tecnico.php
@@ -2,7 +2,6 @@
 <html lang="es">
 <head>
     <?php include('partial_views/head_tag.php') ?>
-    <script src="/assets/scripts/slider.js"></script>
     <title>Carrier Transicold - Servicio Técnico</title>
 </head>
 <body>
@@ -17,8 +16,95 @@
                 <p>Como parte de nuestra propuesta de valor ofrecemos a nuestros clientes servicio técnico profesional para unidades de refrigeración Carrier Transicold.</p>
             </div>
 
-            <div class="img">
+            <div class="img bottom-mobile">
                 <img src="/assets/images/servicio/tecnico-carrier-transicold.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="main-wrapper nuestros-servicios">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Nuestros servicios</h3>
+                <h2>Para todas las necesidades</h2>
+            </div>
+            <div class="boxes-wrapper">
+                <a class="box-shadowed">
+                    <img src="/assets/images/servicio/iconp-mantenimiento.png" alt="">
+                    <div class="description">
+                        <h4>Planes de mantenimiento</h4>
+                        <p>Realizamos mantenimiento preventivo y correctivo para evitar que las unidades sufran un desgaste acelerado, ayudamos a disminuir la probabilidad de futuras fallas.</p>
+                        <button>Solicitar información <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/servicio/icono-servicio.png" alt="">
+                    <div class="description">
+                        <h4>Pólizas de servicio</h4>
+                        <p>Nos encargamos del servicio técnico de tu flotilla a través de nuestro programa de pólizas, recibe un trabajo profesional y garantizado.</p>
+                        <button>Solicitar información <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+                <a class="box-shadowed">
+                    <img src="/assets/images/servicio/icono-garantias.png" alt="">
+                    <div class="description">
+                        <h4>Garantías</h4>
+                        <p>Tenemos la capacidad de cubrir cualquier garantía Carrier Transicold sin importar donde haya sido comprado el equipo dentro de la República Mexicana.</p>
+                        <button>Solicitar información <i class="far fa-long-arrow-right"></i></button>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="main-wrapper gray-wrapper agenda-section-wrapper">
+        <div class="general-max-width">
+            <div class="title-centered">
+                <h3>Nuestros servicios</h3>
+                <h2>Para todas las necesidades</h2>
+            </div>
+            <div class="agenda-grid">
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-vida-equipo.png" alt="">
+                    <p>Extiende la vida de tu equipo</p>
+                </div>
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-reparaciones-emergencia.png" alt="">
+                    <p>Evita costos de reparaciones de emergencia</p>
+                </div>
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-protege-equipo.png" alt="">
+                    <p>Protege tu equipo y tu producto</p>
+                </div>
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-economia-combustible.png" alt="">
+                    <p>Maximiza la economía de combustible</p>
+                </div>
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-aumenta-valor.png" alt="">
+                    <p>Aumenta el valor de reventa</p>
+                </div>
+                <div class="agenda-item">
+                    <img src="/assets/images/servicio/icono-reduce-costos.png" alt="">
+                    <p>Reduce el costo del ciclo de vida del equipo</p>
+                </div>
+            </div>
+            <button class="main-button green-button icon icon-left"><i class="fab fa-whatsapp"></i><span>Agendar mi servicio</span></button>
+        </div>
+    </div>
+
+    <div class="main-wrapper asesoria-wrapper">
+        <div class="general-max-width">
+            <div class="half-section">
+                <img src="/assets/images/servicio/tecnico-especializado-carrier.png" alt="">
+            </div>
+            <div class="half-section">
+                <div class="title-centered">
+                    <h3>Asesoria técnica</h3>
+                    <h2>¿Tienes una duda?<br><span>Nosotros te ayudamos</span></h2>
+                </div>
+                <p>Brindamos asesoría técnica para resolver tus inquietudes en cualquiera de nuestros productos, servicios o refacciones Carrier Transicold.</p>
+                <button class="main-button green-button"><span>Contactar</span></button>
             </div>
         </div>
     </div>
